e9be7ec4dd0312d81bea62b8423ec4adb9891bc9
shalman13091994 <shalman13091994@gmail.com>
2021-04-03 15:37:05 +0530

    for heroku settings and other pip installation

--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -125,6 +130,8 @@ STATICFILES_DIRS=[
 
 ]
 
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFileStorage'
+
 # for Testing email in python  before testing with gmail
 
 # email settings
@@ -146,3 +153,6 @@ EMAIL_USE_TLS=False
 # EMAIL_HOST_PASSWORD='Keypad@1'
 # EMAIL_USE_TLS= True
 # # EMAIL_USE_SSL=False
+
+
+django_heroku.settings(locals())
